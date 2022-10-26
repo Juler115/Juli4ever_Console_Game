aa6e316935a535f96c5a27b92c27e3fbc5567743
Juler115 <erwinalh115@gmail.com>
2022-10-26 16:36:11 -0500

    ultima prueba

--- Cambio.cpp
+++ Cambio.cpp
@@ -17,7 +17,6 @@ void Cambio()
 	int selec;
     if (Datos["Cumple"] == "" || Datos["FavComi"] == "" || Datos["FavPasat"] == "" || Datos["Aspiraciones"] == "")
         cout << "Lo siento, aun no te conozco lo suficiente\n\n";
-
     else {
         do
         {
